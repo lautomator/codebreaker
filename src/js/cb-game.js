85cc84ffc5b590481c682943026ac873522e7f12
lautomator <jmerigliano@gmail.com>
2015-03-06 08:47:22 -0500

    keybaord input in progress

--- src/js/cb-game.js
+++ src/js/cb-game.js
@@ -147,7 +147,7 @@ var codebreaker = function (params, targets) {
                     return ok;
 
                 },
-                keypad_click = function (e, clicks) {
+                keypad_click = function (e) {
 
                     var display = targets.keyDisplay;
 
@@ -177,79 +177,94 @@ var codebreaker = function (params, targets) {
                         display.textContent = clicks.join('');
 
                     }
-
                 },
                 keyboard_input = function (e) {
 
-					var entry = '';
+                    var display = targets.keyDisplay;
 
-					// suppress any key clicks except for numbers, enter, and clear, respectively.
-					if ((e.keyCode >= 48 && e.keyCode <= 57) ||
-							e.keyCode === 13 || e.keyCode === 99) {
+                    if (clicks.length < n_of_guesses &&
+                            keypad_dbls(e, clicks)) {
 
-						// handle 'c' as a reset call.
-						if (e.keyCode === 99) {
+                        // suppress any key clicks except for numbers,
+                        // enter, and clear, respectively.
+                        if (e.keyCode >= 48 && e.keyCode <= 57) {
 
-							entry = 'reset';
-						
-						} else {
-						
-							entry = String.fromCharCode(e.keyCode);
-						
-						}						
-					}
-					
-					console.log(entry);
-					return entry;
-				};
+                            clicks.push(String.fromCharCode(e.keyCode));
+                            targets.playerGuess.value = clicks.join('');
+                            display.textContent = clicks.join('');
+
+                        }
+
+                        // handle 'c' as a reset call.
+                        if (e.keyCode === 99) {
+
+                            console.log('clear');
+
+                            targets.submitGuess.reset();
+                            display.textContent = '';
+                            clicks = '';
+                            targets.playerGuess.value = clicks;
+                            keypad_init();
+
+                        }
+
+                        // return/enter
+                        if (e.keyCode === 13) {
+
+                            console.log(clicks);
+
+                        }
+                    }
+                };
 
             // button mouse click events
-			// 7
+            // 7
             buttons[0].onclick = function () {
-                keypad_click(buttons[0].value, clicks);
+                keypad_click(buttons[0].value);
             };
             // 8
             buttons[1].onclick = function () {
-                keypad_click(buttons[1].value, clicks);
+                keypad_click(buttons[1].value);
             };
             // 9
             buttons[2].onclick = function () {
-                keypad_click(buttons[2].value, clicks);
+                keypad_click(buttons[2].value);
             };
             // 4
             buttons[3].onclick = function () {
-                keypad_click(buttons[3].value, clicks);
+                keypad_click(buttons[3].value);
             };
             // 5
             buttons[4].onclick = function () {
-                keypad_click(buttons[4].value, clicks);
+                keypad_click(buttons[4].value);
             };
             // 6
             buttons[5].onclick = function () {
-                keypad_click(buttons[5].value, clicks);
+                keypad_click(buttons[5].value);
             };
             // 1
             buttons[6].onclick = function () {
-                keypad_click(buttons[6].value, clicks);
+                keypad_click(buttons[6].value);
             };
             // 2
             buttons[7].onclick = function () {
-                keypad_click(buttons[7].value, clicks);
+                keypad_click(buttons[7].value);
             };
             // 3
             buttons[8].onclick = function () {
-                keypad_click(buttons[8].value, clicks);
+                keypad_click(buttons[8].value);
             };
             // 0
             buttons[9].onclick = function () {
-                keypad_click(buttons[9].value, clicks);
+                keypad_click(buttons[9].value);
             };
             // C
             buttons[10].onclick = function () {
-                keypad_click('reset', clicks);
+                keypad_click('reset');
             };
-			
-			window.onkeypress = keyboard_input;
+
+            // keyboard events
+            window.onkeypress = keyboard_input;
 
         },
 
@@ -414,6 +429,7 @@ var codebreaker = function (params, targets) {
                     display.style.visibility = 'hidden';
                     enter.type = 'button';
                     replay.style.visibility = 'visible';
+
                 }
             }
         },
@@ -442,5 +458,3 @@ var codebreaker = function (params, targets) {
     // -------------------------------------
 
 };
-
-// TODO: allow for keyboard input to fire buttons
