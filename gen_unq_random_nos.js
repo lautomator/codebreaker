62216637b6fd0168fdbfd24cf1d1086e81e632f1
g360 User <g360user@iMacProrigliano.at.indyhall.org>
2015-03-23 18:35:10 -0400

    random solution func

--- gen_unq_random_nos.js
+++ gen_unq_random_nos.js
@@ -1,14 +1,58 @@
-function getSample(skill) {
-    
-    // generate a range of 3 random numbers
-    
-    var ranNum;
-    
-    // generate a random number between one and nine
-    ranNum = Math.floor(Math.random() * 10);
-    
-    return ranNum;
-    
+var skill = 3;
+
+function getSample() {
+
+    // generate a sample of three numbers
+    // between 0 and 9.
+
+    var sample = [0, 0, 0];
+
+    for (var index in sample) {
+
+        // generate a random number between one and nine
+        // ranNum = Math.floor(Math.random() * 10);
+
+        sample[index] = Math.floor(Math.random() * 10);
+
+    }
+
+
+
+
+
+
+
+
+
+    // // ensure the numbers are unique
+    // for (var i = 0; i < 3; i++) {
+
+    //     // ensure the numbers are unique
+    //     switch (sample[i]) {
+
+    //         case 0:
+    //             console.log("case 0");
+    //             break;
+
+    //         case 1:
+    //             console.log("case 1");
+    //             break;
+
+    //         case 2:
+    //             console.log("case 2");
+    //             break;
+
+    //         default:
+    //             console.log("default");
+    //             break;
+    //     }
+
+    // }
+
+
+
+    return sample;
+
 }
 
 function setSolution(skill) {
