9d3b1abc8ea47efd008a269aec133b100e140935
John Merigliano <johnmerigliano@Johns-Air.fios-router.home>
2015-04-24 22:38:10 -0400

    initial compile

---
+++ codebreaker/www/js/cb-scores.js
@@ -0,0 +1,30 @@
+function hiScore(ckName, cbBestScoreTarget) {
+
+    "use strict";
+
+    var name = ckName + "=",
+        ckArray = document.cookie.split(';'),
+        topScore = cbBestScoreTarget,
+        l = document.createElement("li"),
+        i;
+
+    for (i = 0; i < ckArray.length; i += 1) {
+
+        while (ckArray[i].charAt(0) === ' ') {
+
+            ckArray[i] = ckArray[i].substring(1);
+
+        }
+
+        if (ckArray[i].indexOf(name) === 0) {
+
+            l.appendChild(document.createTextNode(ckArray[i].substring(
+                name.length,
+                ckArray[i].length
+            )));
+            topScore.appendChild(l);
+        }
+    }
+
+    return "";
+}
